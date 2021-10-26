e5a10844e9af4c1d51c1feaa6d56a52704c85601
Muhammad Diffa Ananda Lukman <diffa.ananda.10@gmail.com>
2021-10-26 15:51:29 +0700

    SplashScreen

---
+++ app/src/main/java/com/ajarin/android/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.ajarin.android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreen, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+}
