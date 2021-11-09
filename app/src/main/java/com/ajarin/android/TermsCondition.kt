d23005b9721270f809b7c7fbc2082543b65f3f01
Muhammad Diffa Ananda Lukman <diffa.ananda.10@gmail.com>
2021-11-09 23:13:38 +0700

    desain privpol & termscon (belum kelar)

---
+++ app/src/main/java/com/ajarin/android/TermsCondition.kt
@@ -0,0 +1,11 @@
+package com.ajarin.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsCondition : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_condition)
+    }
+}
