3292cc2a90e8e9a2a019e71a8f20b6bd7c60d996
Fulky Hariz <fulky.hariz@gmail.com>
2021-11-11 13:48:32 +0700

    benerin register sama login biar bisa nyambung ke database

--- app/src/main/java/com/ajarin/android/Login.kt
+++ app/src/main/java/com/ajarin/android/Login.kt
@@ -2,17 +2,23 @@ package com.ajarin.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.content.Intent
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val registerbtn : TextView = findViewById(R.id.registerbtn)
         val loginbtn : ImageButton = findViewById(R.id.loginbutton)
+        val emailInput : EditText = findViewById(R.id.emailinput)
+        val passwordInput : EditText = findViewById(R.id.passinput)
 
         registerbtn.setOnClickListener{
             Intent( this@Login, Register::class.java).also{
@@ -20,13 +26,32 @@ class Login : AppCompatActivity() {
             }
         }
 
-        loginbtn.setOnClickListener{
-            Intent( this@Login, MainActivity::class.java).also{
-                startActivity(it)
-            }
-        }
-        /*loginbtn.setOnClickListener(
+        auth = FirebaseAuth.getInstance()
 
-        )*/
+        val currentuser = auth.currentUser
+/*        if(currentuser != null) {
+            startActivity(Intent(this@Login, MainActivity::class.java))
+            finish()
+        }*/
+
+        loginbtn.setOnClickListener{
+            if(TextUtils.isEmpty(emailInput.text.toString())){
+                emailInput.setError("Please enter username")
+                return@setOnClickListener
+            }
+            else if(TextUtils.isEmpty(passwordInput.text.toString())){
+                emailInput.setError("Please enter password")
+                return@setOnClickListener
+            }
+            auth.signInWithEmailAndPassword(emailInput.text.toString(), passwordInput.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        startActivity(Intent(this@Login, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(this@Login, "Login failed, please try again! ", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/ajarin/android/Register.kt
+++ app/src/main/java/com/ajarin/android/Register.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.*
-import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -14,31 +13,41 @@ class Register : AppCompatActivity() {
     var databaseReference :  DatabaseReference? = null
     var database: FirebaseDatabase? = null
 
-    val loginbtn : TextView = findViewById(R.id.loginbtn)
-    val registerButton : ImageView = findViewById(R.id.registerbutton)
-    val nameInput : EditText = findViewById(R.id.nameinput)
-    val emailInput : EditText = findViewById(R.id.emailinput)
-    val passwordInput : EditText = findViewById(R.id.passinput)
-    val phoneInput : EditText = findViewById(R.id.phoneinput)
-    val tutorInput : Spinner = findViewById(R.id.stuortuin)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val loginbtn : TextView = findViewById(R.id.loginbtn)
+        val registerButton : ImageView = findViewById(R.id.registerbutton)
+        val nameInput : EditText = findViewById(R.id.nameinput)
+        val emailInput : EditText = findViewById(R.id.emailinput)
+        val passwordInput : EditText = findViewById(R.id.passinput)
+        val phoneInput : EditText = findViewById(R.id.phoneinput)
+        val tutorInput : Spinner = findViewById(R.id.stuortuin)
 
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance()
         databaseReference = database?.reference!!.child("profile")
 
-        register()
-    }
-
-    private fun register() {
-
         registerButton.setOnClickListener {
+
+            if(TextUtils.isEmpty(nameInput.text.toString())) {
+                nameInput.setError("Please enter your name ")
+                return@setOnClickListener
+            } else if(TextUtils.isEmpty(emailInput.text.toString())) {
+                emailInput.setError("Please enter your email ")
+                return@setOnClickListener
+            }else if(TextUtils.isEmpty(phoneInput.text.toString())) {
+                phoneInput.setError("Please enter your phone number ")
+                return@setOnClickListener
+            }else if(TextUtils.isEmpty(passwordInput.text.toString())) {
+                passwordInput.setError("Please enter your password ")
+                return@setOnClickListener
+            }
 
             auth.createUserWithEmailAndPassword(emailInput.text.toString(), passwordInput.text.toString())
                 .addOnCompleteListener {
+
                     if(it.isSuccessful) {
                         val currentUser = auth.currentUser
                         val currentUSerDb = databaseReference?.child((currentUser?.uid!!))
