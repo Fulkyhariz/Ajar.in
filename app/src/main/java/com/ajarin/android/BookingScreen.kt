1c12e7d80ddc340497c0e5de990f1633e9892bb7
fthrr <fathurrahman.irwansa@ui.ac.id>
2021-11-12 09:03:31 +0700

    FE - Kelar keknya, kalo ada revisi bilang

---
+++ app/src/main/java/com/ajarin/android/BookingScreen.kt
@@ -0,0 +1,11 @@
+package com.ajarin.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_booking_screen)
+    }
+}
---
+++ app/src/main/java/com/ajarin/android/DetailedOrder.kt
@@ -0,0 +1,11 @@
+package com.ajarin.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailedOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed_order)
+    }
+}
