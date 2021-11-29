d5aaca389976aaa0036a946586edf066a485c2ac
fthrr <fathurrahman.irwansa@ui.ac.id>
2021-11-29 14:44:43 +0700

    Error - Home

---
+++ app/src/main/java/com/ajarin/android/dataHome.kt
@@ -0,0 +1,12 @@
+package com.ajarin.android
+
+import java.io.Serializable
+
+class dataHome
+    (
+        val topicName: String,
+        val subjectName: String,
+        val tutorName: String
+        ): Serializable
+{
+}
