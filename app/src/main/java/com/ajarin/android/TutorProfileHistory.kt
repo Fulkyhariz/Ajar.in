8e6fc4b7f12c94f76f79f01a67063e95fc90fb34
fthrr <fathurrahman.irwansa@ui.ac.id>
2021-12-10 17:22:20 +0700

    FE dikit

---
+++ app/src/main/java/com/ajarin/android/TutorProfileHistory.kt
@@ -0,0 +1,11 @@
+package com.ajarin.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorProfileHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutor_profile_history)
+    }
+}
